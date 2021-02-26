00bef4b6c05a5c5c90dcc3b65b433ee4e94bd2a0
Brad Malloy <brad@malloy.io>
2021-02-26 15:17:51 -0800

    WIP

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ This game is a roguelike take on the automation strategy genre.
 - Pack for web with Browserify
 - Deploy & host with Netlify
 - rot.js for roguelike tools
+
+## Notes
+
+- Haven't decided on a title yet. Use `$GAME_TITLE` as a placeholder, then we can find/replace later.
+- https://animate.style/ (CSS animations - used sparingly, might be nice)
+- https://oxal.org/projects/sakura/demo/ (minimal CSS)
+
+### Title Ideas
+
+- Frogue (Factory Rogue, logo is a frog?)
+- Factorogue (a little on the nose)
+- Rogue Works
+- Automatr
+- Autorogue
+- Automagic (if it ends up magic-themed?)
+- Romatic
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
 import ROT = require('rot-js');
+import { DisplayOptions } from 'rot-js/lib/display/types';
 
 console.info(ROT.RNG.getPercentage());
 console.info("ROT is loaded correctly!");
+
+var tileSet = document.createElement("img");
+tileSet.src = "mainlevbuild.png";
+
+var options: Partial<DisplayOptions> = {
+    layout: "tile",
+    bg: "transparent",
+    tileWidth: 32,
+    tileHeight: 32,
+    tileSet: tileSet,
+    tileMap: {
+        "@": [0, 0],
+        "#": [0, 64],
+        "a": [64, 0],
+        "!": [64, 64],
+        ".": [192, 672]
+    },
+    width: 3,
+    height: 3
+}
+
+var gameWrapper = document.getElementById("gameCanvas");
+
+var display = new ROT.Display(options);
+gameWrapper.appendChild(display.getContainer());
+
+tileSet.onload = function() {
+    for (let x = 0; x < display.getOptions().width; x++) {
+        for (let y = 0; y < display.getOptions().height; y++) {
+            display.draw(x, y, ".", null, null);
+            console.debug("Drawing '.'");
+        }
+    }
+}
